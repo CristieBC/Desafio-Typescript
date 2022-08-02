c7f132c2cd616f0895e8a545768d6046c8f66c31
Cristie Brandão <cristie.bcastro@gmail.com>
2022-08-02 01:16:38 -0300

    Add files via upload
    Introdução prática ao Typescript

---
+++ app.ts
@@ -0,0 +1,16 @@
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+
+function adicionarNumeros(Numero1: number, Numero2: number, devePrintar: boolean);{
+    if (devePrintar){
+        console.log (Numero1 + Numero2);
+        
+    }
+}
+
+if (button) {
+    button.addEventListener('Click' () => {
+
+    })
+}
---
+++ validaçao-basica.js
@@ -0,0 +1,15 @@
+//let button = document.getElementById('button');
+//let input1 = document.getElementById('input1');
+//let input2 = document.getElementById('input2');
+
+//function somarNumeros (Numero1 , Numero2){
+  //  if ( typeof Numero1 === 'number' && typeof Numero2 === 'number'){
+   //     return Numero1 + Numero2
+
+    //} else {
+   //     return Number (Numero1) + Number (Numero2)
+    //}
+//}
+//button.addEventListener('click', () => {
+  //  console.log(somarNumeros(input1.value, input2.value))
+//})
